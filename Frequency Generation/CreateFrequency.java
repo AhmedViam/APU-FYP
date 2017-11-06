e8057159783212f321fa653a07ad7475fb875b45
Viam <viamtmp@gmail.com>
2017-11-07 00:15:54 +0800

    Added limits constants

--- Frequency Generation/CreateFrequency.java
+++ Frequency Generation/CreateFrequency.java
@@ -1,27 +1,8 @@
 /*
- * The MIT License
- *
- * Copyright 2017 Viam.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
-
 package githubfyp;
 
 import static java.lang.Thread.sleep;
@@ -41,13 +22,14 @@ import java.io.UnsupportedEncodingException;
  *
  * @author Viam
  */
-
 public class FYPCreateFrequency {
 
     
-       protected static final int  SAMPLE_RATE = 64000;
-       protected static final int  ERROR_BITS = 6;
-       protected static final int  SIGNAL_TIMER = 26;
+       protected static final int    SAMPLE_RATE = 64000;
+       protected static final int    ERROR_BITS = 6;
+       protected static final int    SIGNAL_TIMER = 26;
+       protected static final float  LOWER_LIMIT = 200;
+       protected static final float  UPPER_LIMIT = 10000;
    
     
     /**
@@ -60,7 +42,7 @@ public class FYPCreateFrequency {
      */ 
     
   public static byte[] calculateSinWave(double frequency, int timer) throws FYPException {
-        if (frequency >= 10000 || frequency <= 200)
+        if (frequency >= UPPER_LIMIT || frequency <= LOWER_LIMIT)
             throw new FYPException("Frequency is out of bounds");
 
         int samples = (int)((timer * SAMPLE_RATE) / 1000);
